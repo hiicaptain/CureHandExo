79583a0e708dba7a45529432e1ccec8503360166
hiicaptain <yu1997hao@gmail.com>
2026-01-16 08:21:46 +0000

    update: readme

--- Exo_rebuilt/start.sh
+++ Exo_rebuilt/start.sh
@@ -7,7 +7,7 @@ gnome-terminal -- sh -c "roslaunch meta_controller.launch;exec bash"
 sleep 5
 gnome-terminal -- sh -c "python3 sensor_value.py;exec bash"
 sleep 5
-gnome-terminal -- sh -c "python3 ./motor_control_noemg.py;exec bash"
+gnome-terminal -- sh -c "python3 motor_control_noemg.py;exec bash"
 sleep 5
 gnome-terminal -- sh -c "python3 experiment.py;exec bash"
 printf "GUI is running\n"
